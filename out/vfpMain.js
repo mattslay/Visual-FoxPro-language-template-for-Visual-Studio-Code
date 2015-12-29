92516287d3f81171c6fa5025d8bbca7db4d98c30
Matt Slay - MSPC5 <mattslay@jordanmachine.com>
2015-12-29 09:20:40 -0600

    Update to new format required for VS Code

--- out/vfpMain.js
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-'use strict';
-define(["require", "exports", './vfpDef', 'monaco'], function (require, exports, vfpDef, monaco) {
-    monaco.Modes.registerMonarchDefinition('vfp', vfpDef.language);
-});
-//# sourceMappingURL=vfpMain.js.map
